a293b300cf7a7838ae7254862453fd9040523674
Pranaykhetade02 <pranaykhetade152@gmail.com>
2025-11-08 17:23:03 +0530

    images update

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,73 +1,85 @@
 import { NavLink, Link } from "react-router-dom";
-import '../index.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js"; // 🔥 Important for dropdown toggle
+import "../index.css";
+
 const Navbar = () => {
   return (
-    <>
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-  <div className="container-fluid">
-    {/* Brand on the left */}
-    <Link className="navbar-brand text-white" to="/">
-      <span
-        style={{
-          fontFamily: "monospace",
-          borderTop: "2px solid orange",
-          fontSize: "22px",
-          whiteSpace: "nowrap"
-        }}
-      >
-        LiftUp
-      </span>
-      <span
-        style={{
-          color: "orange",
-          fontFamily: "sans-serif",
-          borderBottom: "2px solid white"
-        }}
-      >
-        Fitness
-      </span>
-    </Link>
+      <div className="container-fluid">
+        {/* Brand on the left */}
+        <Link className="navbar-brand text-white" to="/">
+          <span
+            style={{
+              fontFamily: "monospace",
+              borderTop: "2px solid orange",
+              fontSize: "22px",
+              whiteSpace: "nowrap",
+            }}
+          >
+            LiftUp
+          </span>
+          <span
+            style={{
+              color: "orange",
+              fontFamily: "sans-serif",
+              borderBottom: "2px solid white",
+            }}
+          >
+            Fitness
+          </span>
+        </Link>
 
-    {/* Toggler for mobile */}
-    <button
-      className="navbar-toggler"
-      type="button"
-      data-bs-toggle="collapse"
-      data-bs-target="#navbarNav"
-      aria-controls="navbarNav"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span className="navbar-toggler-icon"></span>
-    </button>
+        {/* Toggler button for mobile */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-    {/* Navbar Links */}
-    <div className="collapse navbar-collapse" id="navbarNav">
-      <div className="navbar-nav ms-auto text-center">
-        <NavLink className="nav-item nav-link" to="/" end>
-          Home
-        </NavLink>
-        <NavLink className="nav-item nav-link" to="/about">
-          About
-        </NavLink>
-        <NavLink className="nav-item nav-link" to="/blog">
-          Blogs
-        </NavLink>
-        <NavLink className="nav-item nav-link" to="/gallery">
-          Gallery
-        </NavLink>
-        <NavLink className="nav-item nav-link" to="/service">
-          Service
-        </NavLink>
-        <NavLink className="nav-item nav-link" to="/contact">
-          Contact
-        </NavLink>
+        {/* Navbar Links */}
+        <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+          <ul className="navbar-nav text-center">
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/" end>
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/about">
+                About
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/blog">
+                Blogs
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/gallery">
+                Gallery
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/service">
+                Service
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/contact">
+                Contact
+              </NavLink>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
-  </div>
-</nav>
-
-    </>
+    </nav>
   );
 };
 
